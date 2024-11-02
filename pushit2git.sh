579748373c3e30bec90475b51f1848c88cfd81b1
archn00b <archn00b2024@gmail.com>
2024-11-02 06:52:56 +0000

    changed a cpl of lines

--- pushit2git.sh
+++ pushit2git.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash 
-set -e
+#set -e
 ##################################################################################################################
 # Author 	: ArchN00B
 # Website   : https://www.github.com/ArchN00B
@@ -19,7 +19,7 @@ set -e
 ####################################################################################################################################################
 
 # printing to the screen using color
-printf "\033[34mCHECKING FOR UPDATED FILES ONE REPO\033[m\n"
+printf "\033[34mCHECKING FOR UPDATED FILES ON REPO\033[m\n"
 
 # Checking for updated files on repo
 git pull
